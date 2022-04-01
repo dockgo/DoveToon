54e9e6ebdfc2c0bd772fb8efb99fde45d224a591
ChristianKorompis <95670457+ChristianKorompis@users.noreply.github.com>
2022-04-01 15:32:11 +0800

    Update BookmarkV1.1.js

--- BookmarkV1.1.js
+++ BookmarkV1.1.js
@@ -88,22 +88,36 @@ $(function() {
     if( cartItem && productContainer ){
         productContainer.innerHTML = '';
         Object.values(cartItem).map(item => {productContainer.innerHTML += `
-        <div class="bs">
-        <div data-id="${item.id}" id="${item.id}" class="bsx">
-        <div class="delmark">Delete</div>
-        <a href="${item.ling}" title="${item.title}">
-        <div class="limit">
-        <div class="ply"></div>
-        <span class="type"></span>
-        <img src="${item.borkimage}" class="ts-post-image wp-post-image attachment-medium size-medium" loading="lazy" width="165" height="225"/>
+        <div class="container container-badan-bookmark" data-id="${item.id}">
+ <div class="container container-badan-bookmark-v1">
+  <div tabindex="0" class="container container-badan-post" style="opacity: 1; transform: scale(1);">
+   <div class="container area-thumbnail">
+    <div class="container area-thumbnail-v1">
+     <div class="container thumbnail-post">
+        <img height="100%" loading="eager" src="${item.borkimage}" width="100%" style="object-fit: cover;">
+     <div class="container border-image">
+      <div class="container border-image-v1">
+       <div class="container border-image-v2"></div>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+    <div class="container pembatas-16x16"></div>
+   <div class="container info-post-in-bookmark">
+    <h6 aria-level="6" dir="auto" role="heading" class="css-post-bookmark">${item.title}</h6>
+    <div class="container pembatas-8x8"></div>
+    <div dir="auto" class="css-updated-post">Updated 2022-03-31</div>
+    <div class="container pembatas-8x8"></div>
+    <div dir="auto" class="css-sinopsi"></div>
         </div>
-        <div class="bigor">
-        <div class="tt">${item.title}</div>
-
-        </div>
-        </a>
-        </div>
-        </div>
+  </div>
+  <div class="container pembatas-16x16"></div>
+  <div tabindex="0" class="container button-remove" style="opacity: 1; padding: 4px; transform: scale(1);">
+   <svg fill="#242424" height="28" viewBox="0 0 20 20" width="28" class="r-1q142lx"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.96968 14.9697L5.03034 16.0303L10 11.0607L14.9697 16.0303L16.0303 14.9697L11.0607 10L16.0303 5.03033L14.9697 3.96967L10 8.93934L5.03034 3.96968L3.96968 5.03033L8.93935 10L3.96968 14.9697Z"></path></svg>
+  </div>
+ </div>
+</div>
         `});
     }
  }
